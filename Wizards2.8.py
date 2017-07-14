0a0637bc5706849e8ab9d19c48793eed246808ed
mardini1974 <masounmardini@gmail.com>
2017-07-14 22:36:04 +0200

    Add Center of 3 points
    Added Center for 3 points in 3d space

--- Wizards2.8.py
+++ Wizards2.8.py
@@ -2,11 +2,9 @@
 
 import sys
 import linuxcnc
-import time
 import gtk, gobject
 import numpy as np
 import ConfigParser
-
 
 
 class WizardsApp(gtk.Window):
@@ -19,65 +17,117 @@ class WizardsApp(gtk.Window):
         Pos = 1
         Neg = -1
 
-    XVal_label = gtk.Label()
-    YVal_label = gtk.Label()
-    ValPosX = gtk.Label()
-    ValPosY = gtk.Label()
-    ValNegX = gtk.Label()
-    ValNegY = gtk.Label()
-    Centerresult = gtk.Label()
-    comboincrement = gtk.ComboBox()
-    Feedrate_scale = gtk.HScale()
+    # XVal_label = gtk.Label()
+    # YVal_label = gtk.Label()
+    # ValPosX = gtk.Label()
+    # ValPosY = gtk.Label()
+    # ValNegX = gtk.Label()
+    # ValNegY = gtk.Label()
+    # Centerresult = gtk.Label()
+    # comboincrement = gtk.ComboBox()
+    # Feedrate_scale = gtk.HScale()
+    # C3PX1 = C3PY1 = C3PZ1 = gtk.Label()
+    # C3PX2 = C3PY2 = C3PZ2 = gtk.Label()
+    # C3PX3 = C3PY3 = C3PZ3 = gtk.Label()
 
     def __init__(self):
         builder = gtk.Builder()
         builder.add_from_file("Wizards.glade")
         builder.connect_signals(self)
-        global XVal_label
-        global YVal_label
-        global ValPosX
-        global ValPosY
-        global ValNegX
-        global ValNegY
-        global Centerresult
-        global feedrate
-        global comboincrement
-        global Feedrate_scale
+        global XVal_label, YVal_label
+        global ValPosX, ValPosY, ValNegX, ValNegY, Centerresult, feedrate, comboincrement, Feedrate_scale
+        global C3PX1, C3PY1, C3PZ1
+        global C3PX2, C3PY2, C3PZ2
+        global C3PX3, C3PY3, C3PZ3
+        global C3PCenter,C3PRadius
+
         XVal_label = builder.get_object('XVal')
         YVal_label = builder.get_object('YVal')
         ValPosX = builder.get_object('ValPosX')
         ValPosY = builder.get_object('ValPosY')
         ValNegX = builder.get_object('ValNegX')
+        ValNegY = builder.get_object('ValNegY')
         Centerresult = builder.get_object('Centerresult')
         Feedrate_scale = builder.get_object('hscale1')
         feedrate = getattr(s, 'max_velocity')
+        C3PX1 = builder.get_object('C3PX1')
+        C3PY1 = builder.get_object('C3PY1')
+        C3PZ1 = builder.get_object('C3PZ1')
+        C3PX2 = builder.get_object('C3PX2')
+        C3PY2 = builder.get_object('C3PY2')
+        C3PZ2 = builder.get_object('C3PZ2')
+        C3PX3 = builder.get_object('C3PX3')
+        C3PY3 = builder.get_object('C3PY3')
+        C3PZ3 = builder.get_object('C3PZ3')
+        C3PCenter = builder.get_object('C3PCenter')
+        C3PRadius = builder.get_object('C3PRadius')
+
         window = builder.get_object("window1")
         comboincrement = builder.get_object('Increments')
         gobject.timeout_add(100, self.updateValues)
         comboincrement.set_active(2)
-        ValNegY = builder.get_object('ValNegY')
         try:
             config = ConfigParser.ConfigParser()
             config.read('Wizards.ini')
-            Centerresult.set_text(config.get('ManualCenter','Centerresult'))
-            ValPosX.set_text(config.get('ManualCenter','ValPosX'))
-            ValPosY.set_text(config.get('ManualCenter','ValPosY'))
-            ValNegX.set_text(config.get('ManualCenter','ValNegX'))
-            ValNegY.set_text(config.get('ManualCenter','ValNegY'))
-        except:
+            Centerresult.set_text(config.get('ManualCenter', 'Centerresult'))
+            ValPosX.set_text(config.get('ManualCenter', 'ValPosX'))
+            ValPosY.set_text(config.get('ManualCenter', 'ValPosY'))
+            ValNegX.set_text(config.get('ManualCenter', 'ValNegX'))
+            ValNegY.set_text(config.get('ManualCenter', 'ValNegY'))
+        except IOError:
             Centerresult.set_text('')
-            ValPosX,set_text('')
-            ValPosY,set_text('')
-            ValNegX,set_text('')
-            ValNegY,set_text('')
+            ValPosX.set_text('')
+            ValPosY.set_text('')
+            ValNegX.set_text('')
+            ValNegY.set_text('')
 
         window.set_keep_above(True)
         window.show_all()
 
+    def on_GetP1_clicked(self, *args):
+        s.poll()
+        C3PX1.set_text("{:6.3f}".format(s.position[0] - s.g5x_offset[0]))
+        C3PY1.set_text("{:6.3f}".format(s.position[1] - s.g5x_offset[1]))
+        C3PZ1.set_text("{:6.3f}".format(s.position[2] - s.g5x_offset[2]))
+
+    def on_GetP2_clicked(self, *args):
+        s.poll()
+        C3PX2.set_text("{:6.3f}".format(s.position[0] - s.g5x_offset[0]))
+        C3PY2.set_text("{:6.3f}".format(s.position[1] - s.g5x_offset[1]))
+        C3PZ2.set_text("{:6.3f}".format(s.position[2] - s.g5x_offset[2]))
+
+    def on_GetP3_clicked(self, *args):
+        s.poll()
+        C3PX3.set_text("{:6.3f}".format(s.position[0] - s.g5x_offset[0]))
+        C3PY3.set_text("{:6.3f}".format(s.position[1] - s.g5x_offset[1]))
+        C3PZ3.set_text("{:6.3f}".format(s.position[2] - s.g5x_offset[2]))
+
+    def on_C3PCalc_clicked(self,*args):
+        A = np.array([float(C3PX1.get_text()), float(C3PY1.get_text()), float(C3PZ1.get_text())])
+        B = np.array([float(C3PX2.get_text()), float(C3PY2.get_text()), float(C3PZ2.get_text())])
+        C = np.array([float(C3PX3.get_text()), float(C3PY3.get_text()), float(C3PZ3.get_text())])
+        a = np.linalg.norm(C - B)
+        b = np.linalg.norm(C - A)
+        c = np.linalg.norm(B - A)
+        s = (a + b + c) / 2
+        R = a*b*c / 4 / np.sqrt(s * (s - a) * (s - b) * (s - c))
+        b1 = a*a * (b*b + c*c - a*a)
+        b2 = b*b * (a*a + c*c - b*b)
+        b3 = c*c * (a*a + b*b - c*c)
+        P = np.column_stack((A, B, C)).dot(np.hstack((b1, b2, b3)))
+        P /= b1 + b2 + b3
+        C3PCenter.set_text("[X%s Y%s Z%s]"%("{:6.3f}".format(P[0]),"{:6.3f}".format(P[1]),"{:6.3f}".format(P[2])))
+        C3PRadius.set_text("{:6.3f}".format(R))
+    def on_C3PGotoCenter_clicked(self,*args):
+        c.mode(linuxcnc.MODE_MDI)
+        c.wait_complete()
+        c.mdi("G0 %s" % (C3PCenter.get_text().split("[")[1].split("]")[0]))
+
+
     def updateValues(self):
         s.poll()
-        XVal_label.set_text("{:6.3f}".format(s.position[0]))
-        YVal_label.set_text("{:6.3f}".format(s.position[1]))
+        XVal_label.set_text("{:6.3f}".format(s.position[0] - s.g5x_offset[0]))
+        YVal_label.set_text("{:6.3f}".format(s.position[1] - s.g5x_offset[1]))
         return True
 
     def gtk_main_quit(self, *args):
@@ -103,19 +153,19 @@ class WizardsApp(gtk.Window):
 
     def on_button1_clicked(self, *args):
         s.poll()
-        ValPosX.set_text("{:6.3f}".format(s.position[0]))
+        ValPosX.set_text("{:6.3f}".format(s.position[0] - s.g5x_offset[0]))
 
     def on_button2_clicked(self, *args):
         s.poll()
-        ValNegX.set_text("{:6.3f}".format(s.position[0]))
+        ValNegX.set_text("{:6.3f}".format(s.position[0] - s.g5x_offset[0]))
 
     def on_button3_clicked(self, *args):
         s.poll()
-        ValPosY.set_text("{:6.3f}".format(s.position[1]))
+        ValPosY.set_text("{:6.3f}".format(s.position[1] - s.g5x_offset[1]))
 
     def on_button4_clicked(self, *args):
         s.poll()
-        ValNegY.set_text("{:6.3f}".format(s.position[1]))
+        ValNegY.set_text("{:6.3f}".format(s.position[1] - s.g5x_offset[1]))
 
     def on_window1_key_press_event(self, widget, ev, data=None):
         if ev.keyval == 65363:
@@ -148,7 +198,7 @@ class WizardsApp(gtk.Window):
     def on_GoCenter_clicked(self, *args):
         c.mode(linuxcnc.MODE_MDI)
         c.wait_complete()
-        c.mdi("G53G0 %s" % (Centerresult.get_text().split("[")[1].split("]")[0]))
+        c.mdi("G0 %s" % (Centerresult.get_text().split("[")[1].split("]")[0]))
 
     def on_hscale1_change_value(self, *args):
 
@@ -157,14 +207,12 @@ class WizardsApp(gtk.Window):
     def on_GoCenterX_clicked(self, *args):
         c.mode(linuxcnc.MODE_MDI)
         c.wait_complete()
-        c.mdi("G53G0%s" % (Centerresult.get_text().split("[")[1].split(" Y")[0]))
+        c.mdi("G0%s" % (Centerresult.get_text().split("[")[1].split(" Y")[0]))
 
     def on_GoCenterY_clicked(self, *args):
         c.mode(linuxcnc.MODE_MDI)
         c.wait_complete()
-        c.mdi("G53G0Y%s" % (Centerresult.get_text().split("Y")[1].split("]")[0]))
-
-
+        c.mdi("G0Y%s" % (Centerresult.get_text().split("Y")[1].split("]")[0]))
 
 
 try:
@@ -177,16 +225,18 @@ except linuxcnc.error, detail:
     print "error", detail
     sys.exit(1)
 
+
 def savedata():
-        config = ConfigParser.RawConfigParser()
-        config.add_section('ManualCenter')
-        config.set('ManualCenter', 'Centerresult', Centerresult.get_text())
-        config.set('ManualCenter', 'ValPosX', ValPosX.get_text())
-        config.set('ManualCenter', 'ValPosY', ValPosY.get_text())
-        config.set('ManualCenter', 'ValNegX', ValNegX.get_text())
-        config.set('ManualCenter', 'ValNegY', ValNegY.get_text())
-        with open("Wizards.ini", "w") as outfile:
-            config.write(outfile)
+    config = ConfigParser.RawConfigParser()
+    config.add_section('ManualCenter')
+    config.set('ManualCenter', 'Centerresult', Centerresult.get_text())
+    config.set('ManualCenter', 'ValPosX', ValPosX.get_text())
+    config.set('ManualCenter', 'ValPosY', ValPosY.get_text())
+    config.set('ManualCenter', 'ValNegX', ValNegX.get_text())
+    config.set('ManualCenter', 'ValNegY', ValNegY.get_text())
+    with open("Wizards.ini", "w") as outfile:
+        config.write(outfile)
+
 
 import atexit
 
